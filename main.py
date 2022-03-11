b8d0222eed95401868c405e5ef71a10cc393c192
Matuz Bence <matuzb97@gmail.com>
2022-03-11 11:13:48 +0100

    somewhat working

--- main.py
+++ main.py
@@ -1,81 +1,58 @@
 import cv2
-import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
+import pathlib
 
-import os, sys, time, datetime, random
-import torch
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-from torch.autograd import Variableimport matplotlib.pyplot as plt
-import matplotlib.patches as patches
-from PIL import Image
+import numpy as np
+import tf as tf
 
-
-### pretrained stuff
-
-
-config_path='config/yolov3.cfg'
-weights_path='config/yolov3.weights'
-class_path='config/coco.names'
-img_size=416
-conf_thres=0.8
-nms_thres=0.4# Load model and weights
-model = Darknet(config_path, img_size=img_size)
-model.load_weights(weights_path)
-model.cuda()
-model.eval()
-classes = utils.load_classes(class_path)
-Tensor = torch.cuda.FloatTensor
 
 
 
 Video_FILE = "img1.mp4"
 
-vidcap=cv2.VideoCapture(Video_FILE)
 
-cap = cv2.VideoCapture(Video_FILE)
-
-while cap.isOpened():
-
-    ret, frame = cap.read()
-##########
+cascade_src = 'cars.xml'
+# video = 'data/Cars_On_Highway.mp4'
+video = Video_FILE
 
 
-    # if frame is read correctly ret is True
-    # setup initial location of window
-    r, h, c, w = 250, 90, 400, 125  # simply hardcoded the values
-    track_window = (c, r, w, h)
-    # set up the ROI for tracking
-    roi = frame[r:r + h, c:c + w]
-    hsv_roi = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv_roi, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
-    roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
-    cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+# video = 'data/video2.avi'
 
-    # Setup the termination criteria, either 10 iteration or move by atleast 1 pt
-    term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
 
-    if ret == True:
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+def detectCars(filename):
+    rectangles = []
+    cascade = cv2.CascadeClassifier(cascade_src)
 
-        # apply meanshift to get the new location
-        ret, track_window = cv2.meanShift(dst, track_window, term_crit)
+    vc = cv2.VideoCapture(filename)
 
-        # Draw it on image
-        x, y, w, h = track_window
-        img2 = cv2.rectangle(frame, (x, y), (x + w, y + h), 255, 2)
-        cv2.imshow('img2', img2)
-
-        k = cv2.waitKey(60) & 0xff
-        if k == 27:
-            break
-        else:
-            cv2.imwrite(chr(k) + ".jpg", img2)
-
+    if vc.isOpened():
+        rval, frame = vc.read()
     else:
-        break
+        rval = False
 
-cv2.destroyAllWindows()
-cap.release()
+    while rval:
+        rval, frame = vc.read()
+        frameHeight, frameWidth, fdepth = frame.shape
+
+        # Resize
+        frame = cv2.resize(frame, (600, 400))
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # haar detection.
+        cars = cascade.detectMultiScale(gray, 1.3, 3)
+
+        for (x, y, w, h) in cars:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        # show result
+        cv2.imshow("Result", frame)
+
+        if cv2.waitKey(33) == ord('q'):
+            break
+
+    vc.release()
+
+
+detectCars(video)
